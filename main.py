3922db45b9700f582816b128176f9d58d70371ac
Aar220 <aaryanboreddy722@gmail.com>
2024-07-01 23:44:20 -0700

    CH3L4_Done

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def main():
     x = int(input('Enter your input: '))
     y = int(input('Enter your input: '))
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+
+    if x > 0 and y > 0:
+        quadrant = 1
+    elif x < 0 and y > 0:
+        quadrant = 2
+    elif x < 0 and y < 0:
+        quadrant = 3
+    else:
+        quadrant = 4
+ 
 
     print(f'Quadrant: {quadrant}')
     ########################################
